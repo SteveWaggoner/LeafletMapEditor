6c0e92208231f65e0c8dd0ccc59a794f7f5b46f3
Steve Waggoner <stwaggoner@gmail.com>
2019-06-09 12:26:50 -0700

    add geographic functions for distance and pnt-in-poly

--- browser_app/src/app/app.component.ts
+++ browser_app/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 import { tileLayer, latLng } from 'leaflet';
+
+import {Coordinate} from "tsgeo/Coordinate";
+import {Vincenty}   from "tsgeo/Distance/Vincenty";
+import {Polygon}   from "tsgeo/Polygon";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
+  })
+
+export class AppComponent implements AfterViewInit {
   title = 'browser-app';
 
   columnDefs = [
@@ -28,5 +33,32 @@ export class AppComponent {
 	    ],
 	    zoom: 5,
 	    center: latLng(46.879966, -121.726909)
-    };
+        };
+
+  ngAfterViewInit() {
+
+
+    /* Add the following in a method of TS class */
+
+    let coordinate1 = new Coordinate(19.820664, -155.468066); // Mauna Kea Summit
+    let coordinate2 = new Coordinate(20.709722, -156.253333); // Haleakala Summit
+
+    console.log(coordinate1.getDistance(coordinate2, new Vincenty())); // returns 128130.850 (meters; ≈128 kilometers)
+
+
+    /* Add the following in a method of TS class */
+    let geofence = new Polygon();
+
+    geofence.addPoint(new Coordinate(-12.085870,-77.016261));
+    geofence.addPoint(new Coordinate(-12.086373,-77.033813));
+    geofence.addPoint(new Coordinate(-12.102823,-77.030938));
+    geofence.addPoint(new Coordinate(-12.098669,-77.006476));
+
+    let outsidePoint = new Coordinate(-12.075452, -76.985079);
+    let insidePoint = new Coordinate(-12.092542, -77.021540);
+
+    console.log(geofence.contains(outsidePoint)); // returns bool(false) the point is outside the polygon
+    console.log(geofence.contains(insidePoint)); // returns bool(true) the point is inside the polygon
+
+  }
 }
